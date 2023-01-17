4997a87ed7cca08475d0a35008f874baa7d36973
Tnozone <maxnocquet@gmail.com>
2023-01-17 03:17:48 -0800

    syntax

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ last = number % 10
 print(f"The last digit of {number} is {last} ")
 if last > 5:
     print("and is greater than 5")
-elif last = 0:
+elif last == 0:
     print("and is 0")
 else:
     print("and is less than 6 and not 0")
