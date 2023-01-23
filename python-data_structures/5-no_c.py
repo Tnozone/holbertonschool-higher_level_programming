e09b9583c16aff5ea35bbcd802c54f4447caee54
Tnozone <maxnocquet@gmail.com>
2023-01-23 05:08:30 -0800

    my string

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new = list(my_list)
+    new = list(my_string)
     for x in new:
-        if x=='c' or x=='C':
-            x==''
+        if x == 'c' or x == 'C':
+            x == ''
     return new
