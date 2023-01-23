71b1024fc2ff8ff8f1c4750c7bc78475262eb66c
Tnozone <maxnocquet@gmail.com>
2023-01-23 06:16:58 -0800

    no indent

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,6 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             print("{:d}".format(matrix[i][j]),
-            end=" " if j < len(matrix[i])-1 else "")
+end=" " if j < len(matrix[i])-1 else "")
             if j == len(matrix[i])-1:
                 print()
