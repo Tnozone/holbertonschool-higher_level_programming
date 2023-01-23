78393b9633fd1755e3d8117b45ed0ac6feabbac9
Tnozone <maxnocquet@gmail.com>
2023-01-23 04:57:33 -0800

    order of things

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    a = my_list[:]
     if idx < 0 or idx > len(my_list)-1:
         return my_list
     else:
-        a = my_list
         a[idx] = element
         return a
