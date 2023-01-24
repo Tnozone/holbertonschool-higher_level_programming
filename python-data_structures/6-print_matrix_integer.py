8b7329e5b8f02be7002e7424148272cae84dbb32
Tnozone <maxnocquet@gmail.com>
2023-01-24 04:12:59 -0800

    fix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix == [[]]:
-        print()
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=" " if j < len(matrix[i])-1 else "")
-            if j == len(matrix[i])-1:
+            if j != 0:
+                print(" ", end="")
+            print("{:d}".format(matrix[i][j]), end="")
                 print()
