8f0db7c5c50bf9abc85c01df014e8a18426437b3
Tnozone <maxnocquet@gmail.com>
2023-01-26 01:49:34 -0800

    indentation

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -13,10 +13,10 @@ def roman_to_int(roman_string):
      else:
          res = 0
 
-    dict = {"IV": 4, "I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
-            "D": 500, "M": 1000}
-    for i in roman_string:
-        for j in dict:
-            if j == i:
-                res += dict[j]
-    return res
+        dict = {"IV": 4, "I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
+                "D": 500, "M": 1000}
+        for i in roman_string:
+            for j in dict:
+                if j == i:
+                    res += dict[j]
+        return res
