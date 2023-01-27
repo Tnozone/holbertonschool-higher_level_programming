da0acddaeb2827943018d41e4a4fe0a6de14ad8c
Tnozone <maxnocquet@gmail.com>
2023-01-27 04:11:15 -0800

    except correct

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    c = 0
     try:
         c = a / b
-    except:
-        return None
+        return c
+    except ZeroDivisionError:
+        c = None
     finally:
         print("Inside result: {}".format(c))
-        return c
