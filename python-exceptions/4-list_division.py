d4285471333c6e2287ac3f585d62b689e13d0988
Tnozone <maxnocquet@gmail.com>
2023-01-27 04:53:00 -0800

    remove extra return

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -4,15 +4,14 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         try:
             div = my_list_1[i] / my_list_2[i]
-            return divlist
-        except TypeError:
-            print("wrong type")
+        except IndexError:
+            print("out of range")
             div = 0
         except ZeroDivisionError:
             print("division by 0")
             div = 0
-        except IndexError:
-            print("out of range")
+        except TypeError:
+            print("wrong type")
             div = 0
         finally:
             divlist.append(div)
