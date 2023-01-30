b286c4440a94e6ee07ee7f275b4609a0242f3cef
Tnozone <maxnocquet@gmail.com>
2023-01-30 11:20:49 -0800

    access update

---
+++ python-classes/4-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" class module documentation """
+
+class Square:
+    """ Square class with private instance attribute size """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
