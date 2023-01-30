de135dce6fa0271708b8900434fbddf8401609f6
Tnozone <maxnocquet@gmail.com>
2023-01-30 05:02:50 -0800

    altered doc

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,17 +3,12 @@
 
 
 class Square:
-    """define a Square class
+    """define a Square class.
     Args:
         size (int): Size of the square.
 
     Attributes:
         __size (int): Private size of the square.
     """
-    def __init__(self, size)
-        """Initialize the square with size.
-
-        Args:
-            size (int): Size of the square.
-        """
+    def __init__(self, size)        
         self.__size = size
