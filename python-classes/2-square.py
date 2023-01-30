5b3d31fe164bec3b5d15fb42427eb219952e266c
Tnozone <maxnocquet@gmail.com>
2023-01-30 06:20:25 -0800

    class doc

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,6 +3,7 @@
 
 
 class Square:
+    """ Square class with private instance attribute size """
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
