d7da73c6dad7ac3d925cf974cabe9dedc5ff5cc3
Tnozone <maxnocquet@gmail.com>
2023-02-02 01:15:40 -0800

    elif else

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,6 +7,7 @@ def add_integer(a, b=98):
 
     if a is not int or a is not float:
         raise TypeError("a must be an integer")
-    if b is not int or b is not float:
+    elif b is not int or b is not float:
         raise TypeError("b must be an integer")
-    return (int(a) + int(b))
+    else:
+        return (int(a) + int(b))
