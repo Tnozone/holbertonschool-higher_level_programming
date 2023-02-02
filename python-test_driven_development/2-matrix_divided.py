c85bfbee34972d46cdec86ab6bed291605114c7d
Tnozone <maxnocquet@gmail.com>
2023-02-02 02:03:31 -0800

    error messages

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,4 +5,13 @@
 def matrix_divided(matrix, div):
     """ matrix division function """
 
+    if not all(isinstance(x, list) for x in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(len(x) == len(matrix[0]) for x in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
     return [[round(x / div, 2) for x in list] for list in matrix]
