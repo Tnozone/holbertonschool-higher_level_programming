ac0e0e4391263624e1a365fe44f65bf1c263511f
Tnozone <maxnocquet@gmail.com>
2023-02-02 01:32:38 -0800

    none case

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,7 +5,7 @@
 def add_integer(a, b=98):
     """ addition function """
 
-    if type(a) is not int and type(a) is not float:
+    if a is None or (type(a) is not int and type(a) is not float):
         raise TypeError("a must be an integer")
     elif type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
