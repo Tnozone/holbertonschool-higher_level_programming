f322d884c9def2c706916c4e4c559779c37a75fa
Tnozone <maxnocquet@gmail.com>
2023-02-02 03:53:06 -0800

    fix?

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,7 +5,7 @@
 def matrix_divided(matrix, div):
     """ matrix division function """
 
-    if not all(isinstance(x, list) for x in matrix):
+    if not all(isinstance(el, list) for el in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if not all(len(x) == len(matrix[0]) for x in matrix):
         raise TypeError("Each row of the matrix must have the same size")
