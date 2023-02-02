76dc1db4cd65e6c9d3d0730026cc67e54bca554b
Tnozone <maxnocquet@gmail.com>
2023-02-02 01:57:55 -0800

    syntax

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ module documentation """
 
+
 def matrix_divided(matrix, div):
     """ matrix division function """
 
-    return [[round(x / div, 2) for x in list] ofr list in matrix]
+    return [[round(x / div, 2) for x in list] for list in matrix]
