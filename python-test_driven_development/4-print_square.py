cf776d78eac47e78fa8cb5ed7baf6b6155b8b39f
Tnozone <maxnocquet@gmail.com>
2023-02-02 04:25:05 -0800

    order

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -5,14 +5,14 @@
 def print_square(size):
     """ print square function """
 
-    if type(size) is not int:
+    if not isinstance(size, int) and not isinstance(size, float):
+        raise TypeError("size must be an integer")
+    if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if type(size) is float and size is < 0:
-        raise TypeError("size must be an integer")
 
-    for x in size:
-        for y in size:
-            print("#")
-        print()
+    if isinstance(size, float):
+        size = int(size)
+    for i in range(size):
+        print('#' * size)
