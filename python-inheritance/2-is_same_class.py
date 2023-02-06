bfacd049946adf72b081cb9c16b6c9ab7a937746
Tnozone <maxnocquet@gmail.com>
2023-02-06 06:39:07 -0800

    returns

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,4 +5,7 @@
 def is_same_class(obj, a_class):
     """ function documentation """
 
-    return isinstance(obj, a_class)
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
