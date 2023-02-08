f2faed0bd37972a64099828f769649da647be71d
Tnozone <maxnocquet@gmail.com>
2023-02-08 10:13:39 -0800

    remove area

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -13,8 +13,5 @@ class Square(Rectangle):
         super().__init__(size, size)
         self.__size = size
 
-    def area(self):
-        return self.__size * 2
-
     def __str__(self):
         return "[Square] {}/{}".format(self.__size, self.__size)
