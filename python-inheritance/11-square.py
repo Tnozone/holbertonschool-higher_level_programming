a3cf12931ad74141acb6c91a953f9c7b27932611
Tnozone <maxnocquet@gmail.com>
2023-02-08 10:12:02 -0800

    square area

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Class Module """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square class """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * 2
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size)
