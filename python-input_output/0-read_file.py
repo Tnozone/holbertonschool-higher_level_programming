0391d650b14fa332eedeb6d29ad0c306dad202a5
Tnozone <maxnocquet@gmail.com>
2023-02-09 00:55:39 -0800

    read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Class Module """
+
+
+def read_file(filename=""):
+    """ read file function """
+
+    with open(filename, "r") as f
+        f = f.read
+        print(f, end="")
