a9d79d4701c603aaa70dda2705ea54f31bdccfdc
Tnozone <maxnocquet@gmail.com>
2023-02-09 01:51:54 -0800

    more student

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Class Module """
+
+
+class Student:
+    """ student class """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            dict = {}
+            for key, value in self.__dict__.items():
+                for el in attrs:
+                    if key == el:
+                        dict[key] = value
+            return dict
+        else:
+            return self.__dict__
