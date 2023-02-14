87c5c2a17f8dfee8254e66688c66b45381c5bc2a
Tnozone <maxnocquet@gmail.com>
2023-02-14 05:32:28 -0800

    dictionary

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -105,3 +105,9 @@ class Rectangle(Base):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """returns representation"""
+
+        return {'id': self.id, 'width': self.width,
+                'height': self.height, 'x': self.x, 'y': self.y}
