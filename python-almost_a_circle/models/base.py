4aea12dc16dfc7a6360f72244a49b759fe627b6c
Tnozone <maxnocquet@gmail.com>
2023-02-14 06:03:50 -0800

    json dumps

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -13,3 +13,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Returns JSON Representation"""
+
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
