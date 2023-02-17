38ff207596d8f93a939a900e25a3d41c1ca60414
Tnozone <maxnocquet@gmail.com>
2023-02-17 05:19:42 -0800

    redo rectangle

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import unittest
 
 from models.rectangle import Rectangle
@@ -18,6 +19,30 @@ class TestRectangle(unittest.TestCase):
         elf.assertEqual(r.x, 30)
         self.assertEqual(r.y, 40)
         self.assertEqual(r.id, 50)
+
+    def test_type_error_init(self):
+        self.assertRaisesRegex(TypeError, "width must be an integer",
+                Rectangle, "8", 6, 4, 2, 10)
+        self.assertRaisesRegex(TypeError, "height must be an integer",
+                Rectangle, 8, "6", 4, 2, 10)
+        self.assertRaisesRegex(TypeError, "x must be an integer",
+                Rectangle, 8, 6, "4", 2, 10)
+        self.assertRaisesRegex(TypeError, "y must be an integer",
+                Rectangle, 8, 6, 4, "2", 10)
+
+    def test_value_error_init(self):
+        self.assertRaisesRegex(ValueError, "width must be > 0",
+                Rectangle, -8, 6, 4, 2, 10)
+        self.assertRaisesRegex(ValueError, "width must be > 0",
+                Rectangle, 0, 6, 4, 2, 10)
+        self.assertRaisesRegex(ValueError, "height must be > 0",
+                Rectangle, 2, 0, 4, 2, 10)
+        self.assertRaisesRegex(ValueError, "height must be > 0",
+                Rectangle, 8, -6, 4, 2, 10)
+        self.assertRaisesRegex(ValueError, "x must be >= 0",
+                Rectangle, 8, 6, -4, 2, 10)
+        self.assertRaisesRegex(ValueError, "y must be >= 0",
+                Rectangle, 8, 6, 4, -2, 10)
 
     def test_width_positive_integer(self):
         r = Rectangle(10, 2)
@@ -55,11 +80,54 @@ class TestRectangle(unittest.TestCase):
         r = Rectangle(14, 16, 12, 15, 25)
         self.assertEqual(str(r), "[Rectangle] (25) 12/15 - 14/16")
 
-    def test_update(self):
-        r = Rectangle(1, 2, 3, 4, 5)
-        r.update(10, 20, 30, 40, 50)
-        self.assertEqual(r.id, 10)
+    def test_update_args(self):
+        r = Rectangle(10, 10, 10, 10)
+        r.update(89)
+        self.assertEqual(r.id, 89)
+        r.update(89, 20)
         self.assertEqual(r.width, 20)
-        self.assertEqual(r.height, 30)
-        self.assertEqual(r.x, 40)
-        self.assertEqual(r.y, 50)
+
+    def test_update_kwargs(self):
+        r = Rectangle(10, 10, 10, 10)
+        r.update(height=1)
+        self.assertEqual(r.height, 1)
+
+    def test_to_dictionary(self):
+        r = Rectangle(2, 4, 6, 8, 22)
+        self.assertEqual(r.to_dictionary(), {'id': 22, 'width': 2,
+            'height': 4, 'x': 6, 'y': 8})
+
+    def test_create(self):
+        r = Rectangle.create(**{'id': 1, 'width': 3,'height': 5,
+            'x': 0, 'y': 0})
+        r1 = Rectangle(1, 3, 5)
+        self.assertIsNot(r, r1)
+
+    def test_save_to_file(self):
+        r = Rectangle(10, 7, 2, 8, 1)
+        Rectangle.save_to_file(None)
+        with open("Rectangle.json", "r") as f:
+            read = f.read()
+        self.assertEqual(read, '[]')
+
+        Rectangle.save_to_file([r])
+        with open("Rectangle.json", "r") as f:
+            read = f.read()
+        result = '[{"id": 1, "width": 10, "height": 7, "x": 2, "y": 8}]'
+        self.assertEqual(read, result)
+
+        Rectangle.save_to_file([])
+        with open("Rectangle.json", "r") as f:
+            read = f.read()
+        self.assertEqual(read, '[]')
+
+    def test_load_from_file(self):
+        r = Rectangle(5, 8)
+        Rectangle.save_to_file([r])
+        rectangles = Rectangle.load_from_file()
+        self.assertIsInstance(rectangles[0], Rectangle)
+        self.assertEqual(rectangles[0].width, 5)
+        self.assertEqual(rectangles[0].height, 8)
+
+if __name__ == '__main__':
+    unittest.main()
