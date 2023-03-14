84af18a8e31e7a663e79a6e42cc4bbf1e50926a6
Tnozone <maxnocquet@gmail.com>
2023-03-14 06:22:53 -0700

    password string

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- creates the MySQL server user.
 CREATE USER IF NOT EXISTS user_0d_1@localhost;
-SET PASSWORD FOR user_0d_1@localhost = user_0d_1_pwd;
+SET PASSWORD FOR user_0d_1@localhost = 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
 FLUSH PRIVILEGES;
