665eb57b2f2bfcb9479da6fa46c6386184e108f2
Tnozone <maxnocquet@gmail.com>
2023-03-16 02:12:58 -0700

    typo fix

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,8 +1,10 @@
 -- creates the database and the table.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CRAETE TABLE IF NOT EXISTS cities (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	state_id INT NOT NULL FOREIGN KEY REFERENCES states(id),
-	name VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS cities (
+	id INT NOT NULL AUTO_INCREMENT,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id) REFERENCES states(id)
 );
