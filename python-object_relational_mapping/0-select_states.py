730bd6e88f5e1361e629cef55a92f3a3915e900a
Tnozone <maxnocquet@gmail.com>
2023-03-17 05:21:15 -0700

    template

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-
-
 """ Lists all states from the database sorted in ascending order """
 
 
@@ -10,41 +7,17 @@ import sys
 
 
 if __name__ == "__main__":
-    """ main fucntion """
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    db = MySQLdb.connect(host="localhost",
+    conn = MySQLdb.connect(
+            host="localhost",
             port=3306,
-            user=username,
-            passwd=password,
-            db=database)
-
-    cursor = db.cursor()
-
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    rows = cursor.fetchall()
-    for row in rows:
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            database=sys.argv[3]
+            )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
-
-    cursor.close()
-    db.close()
-
-
-import MySQLdb
-from sys import argv
-
-
-if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', user=argv[1], passwd=argv[2],
-            db=argv[3])
-    st = db.cursor()
-    st.execute("""SELECT `id`, `name` FROM states ORDER BY `id`;""")
-    res = st.fetchall()
-    for i in res:
-        print(i)
-    db.close()
->>>>>>> 143f3f0714e0d3ad05316566215efe56a7832bcf
+    cur.close()
+    conn.close()
