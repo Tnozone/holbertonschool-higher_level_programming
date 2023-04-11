2332b1971cc7e400d4a13c2604049e113fada75d
Tnozone <maxnocquet@gmail.com>
2023-04-11 05:04:43 -0700

    space

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const arr1 = ['C is fun','Python is cool','JavaScript is amazing'];
+const arr1 = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (const strs of arr1) {
 	console.log(strs);
