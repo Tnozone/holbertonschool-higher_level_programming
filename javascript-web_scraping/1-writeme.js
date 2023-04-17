1e279100ac4393b20f80207512724cfef890857c
Tnozone <maxnocquet@gmail.com>
2023-04-17 04:25:50 -0700

    task 1

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filepath = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(filepath, string, 'utf-8', (error) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+});
